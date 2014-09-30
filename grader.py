eec013df2ada9f7bd6de0b25ab4fd2006d49266b
Sanghee Kim <sangheestyle@gmail.com>
2014-09-29 21:33:44 -0600

    refactoring: seperate homework class from the other

--- grader.py
+++ grader.py
@@ -3,24 +3,7 @@ import urllib2
 import re
 import difflib
 from github import Github
-
-
-class Homework:
-    def __init__(self, email, login, title, submitted_at, commits, files):
-        self.email = email
-        self.login = login
-        self.title = title
-        self.submitted_at = submitted_at
-        self.commits = commits
-        self.files = files
-        self.answer_urls = []
-        self.answer_sheets = []
-        self.scores = []
-        self.ambiguity = False
-        self.final_score = 0
-
-    def set_final_scores(self):
-        self.final_score = max(self.scores)
+from homework import Homework
 
 
 class Grader:
