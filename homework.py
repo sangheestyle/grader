2360acfcda50aaaa5e91c4e6dae417dd305e2729
Sanghee Kim <sangheestyle@gmail.com>
2014-09-30 00:12:47 -0600

    set expected score

--- homework.py
+++ homework.py
@@ -1,3 +1,5 @@
+import re
+
 class Homework:
     def __init__(self, email, login, title, submitted_at, commits, files):
         self.name = None
@@ -9,6 +11,7 @@ class Homework:
         self.files = files
         self.answer_urls = []
         self.answer_sheets = []
+        self.expected_score = 0
         self.scores = []
         self.ambiguity = False
         self.final_score = 0
@@ -18,9 +21,27 @@ class Homework:
 
     def set_name(self):
         names = []
+
         for answer_sheet in self.answer_sheets:
             for index in range(len(answer_sheet)):
                 if answer_sheet[index]['head_contents'] == '# Name':
                     names.append(answer_sheet[index]['main_contents'])
 
         self.name = max(names)
+
+    def set_expected_score(self):
+        scores = []
+        regex = r"^([0-9]{1,3})/[0-9]{1,3}.*"
+        rx = re.compile(regex)
+
+        for answer_sheet in self.answer_sheets:
+            for index in range(len(answer_sheet)):
+                head_contents = answer_sheet[index]['head_contents']
+
+                if head_contents.startswith('# How many points'):
+                    result = rx.search(answer_sheet[index]['main_contents'])
+
+                    if result is not None:
+                        scores.append(int(result.group(1)))
+
+        self.expected_score = max(scores)
--- tests.py
+++ tests.py
@@ -13,7 +13,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.hw = Homework(email, login, title, submitted_at, commits, files)
         self.hw.answer_sheets = [{0:{'main_contents': 'First Last',
                                      'head_contents': '# Name'},
-                                  1:{'main_contents': "100/100(Make your",
+                                  1:{'main_contents': "90/100(Make your",
                                      'head_contents': '# How many points'},
                                   2:{'main_contents': 'http://imgur.com/mC2uo',
                                      'head_contents': '## Checkpoint 1 (10 points)'}}
@@ -22,6 +22,11 @@ class TestSequenceFunctions(unittest.TestCase):
     def test_set_name(self):
         self.hw.set_name()
         self.assertEqual(self.hw.name, 'First Last')
+
+    def test_set_expected_score(self):
+        self.hw.set_expected_score()
+        self.assertEqual(self.hw.expected_score, 90)
+
 
 if __name__=='__main__':
     unittest.main()
