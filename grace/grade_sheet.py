0061334403aaaeca0d9bb157e125402ef7683dac
Sanghee Kim <sangheestyle@gmail.com>
2014-11-02 22:29:42 -0700

    save brief report into excel file

--- grace/grade_sheet.py
+++ grace/grade_sheet.py
@@ -1,5 +1,7 @@
 import pickle
 from collections import defaultdict
+import pandas as pd
+
 
 class GradeSheet:
     def __init__(self):
@@ -22,3 +24,28 @@ class GradeSheet:
     def read_pickle(self, file_name):
         with open(file_name, "rb") as fp:
             self._grade = pickle.load(fp)
+
+    def to_excel(self, file_name):
+        brief_df = self.get_brief()
+        brief_df.to_excel(file_name)
+
+    def get_brief(self):
+        base_df = pd.DataFrame()
+        for week in self._grade:
+            index = week
+            logins = []
+            scores = []
+            for entry in self._grade[week]:
+                logins.append(entry['login'])
+                expected = entry['expected']
+                score = entry['score']
+                scores.append(max(expected, score))
+            week_sheet = pd.DataFrame([scores], columns=logins, index=[index])
+            base_df = pd.concat([base_df, week_sheet])
+        return base_df
+
+
+if __name__=="__main__":
+    a = GradeSheet()
+    a.read_pickle('../scored/grade_sheet1-9.pkl')
+    a.to_excel('1-9.xls')
